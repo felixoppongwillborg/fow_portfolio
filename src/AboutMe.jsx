05b74c95e690388b9e6777447ede93fd3df1b785
felixoppongwillborg <fwlixw@gmail.com>
2020-07-27 15:42:43 +0200

    added About me page with blurring modal

---
+++ src/AboutMe.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Button, Modal } from "semantic-ui-react";
+
+class ModalExampleDimmer extends Component {
+  state = { open: false };
+
+  show = (dimmer) => () => this.setState({ dimmer, open: true });
+  close = () => this.setState({ open: false });
+
+  render() {
+    const { open, dimmer } = this.state;
+
+    return (
+      <div>
+        <Button onClick={this.show("blurring")}>About me</Button>
+
+        <Modal dimmer={dimmer} open={open} onClose={this.close}>
+          <Modal.Header>Felix Oppong Willborg</Modal.Header>
+          <Modal.Content image>
+            <Modal.Description>
+              <p>
+                Hello, My name is Felix i'm a bilingual Junior Web Developer,
+                with previous experience within the insurance, retail and art
+                industry.
+              </p>
+              <p>
+                During the past 10 years, I have acquired a broad set of both
+                hard and soft skills applicable across different industries.
+              </p>
+              <p>
+                Having extensive experience working in B2B and B2C roles, can
+                create and test codes for apps and web development.
+              </p>
+              <p>
+                Hands-on, innovative and a problem solver with keen attention to
+                details.
+              </p>
+            </Modal.Description>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color="black" onClick={this.close}>
+              Close
+            </Button>
+            <Button
+              positive
+              icon="checkmark"
+              labelPosition="right"
+              content="Contact me"
+              onClick={this.close}
+            />
+          </Modal.Actions>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default ModalExampleDimmer;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AboutMe from './AboutMe';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <AboutMe />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('app')
 );
 
 // If you want your app to work offline and load faster, you can change
